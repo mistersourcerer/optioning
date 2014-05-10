478e180f175cc91f861c19a0447ebdd77c073171
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2014-05-10 15:13:00 +0000

    updates the example to include callee info

--- examples/module_maroto.rb
+++ examples/module_maroto.rb
@@ -4,6 +4,6 @@ module Maroto
   def hasherize(*values_and_options)
     optioning = Optioning.new values_and_options
     optioning.deprecate :to_hash, :to, "v2.0.0"
-    optioning.deprecation_warn
+    optioning.deprecation_warn caller
   end
 end
