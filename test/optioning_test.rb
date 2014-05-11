230de91db0967ae69e985a8d8833f3c59c44be50
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2014-05-11 16:56:35 +0000

    [WIP] note to self: testing the general process flow

--- test/optioning_test.rb
+++ test/optioning_test.rb
@@ -2,6 +2,34 @@ describe Optioning do
   let(:to_hash_lambda) { ->(value) { value } }
   let(:optioning) { Optioning.new :path, :commit, to_hash: to_hash_lambda }
 
+  before do
+    @original_stderr, $stderr = $stderr, StringIO.new
+  end
+
+  after do
+    $stderr = @original_stderr
+  end
+
+  describe "#process" do
+    it "returns the instance of `Optioning`" do
+      optioning.process.must_be_same_as optioning
+    end
+
+    it "shows deprecations and unrecognized warnings" do
+      optioning = Optioning.new :path, :commit,
+        old: "OH!",
+        from: ->(){},
+        wtf: "?",
+        omg: "O YEAH!"
+
+      optioning.deprecate :old, :new
+      optioning.deprecate :from_hash, :from
+      optioning.recognize :omg
+
+      false.must_be :==, true, "testando aqui a interface geral..."
+    end
+  end
+
   describe "#raw" do
     it "transforms the var args passed to the constructor into an Array" do
       optioning.raw.must_be :==, [:path, :commit, to_hash: to_hash_lambda]
@@ -18,12 +46,6 @@ describe Optioning do
     it "returns the value passed to specific option" do
       # impossible to use lambda.must_be_same_as, maybe a bug?
       optioning.on(:to_hash).object_id.must_be :==, to_hash_lambda.object_id
-    end
-  end
-
-  describe "#process" do
-    it "returns the instance of `Optioning`" do
-      optioning.process.must_be_same_as optioning
     end
   end
 end
