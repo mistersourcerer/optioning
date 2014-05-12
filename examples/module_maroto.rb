9e0d5d4cbbd0d9dc6db41b63d7e4c6f04377e909
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2014-05-12 15:00:48 -0300

    A more out of the box/usable example.

--- examples/module_maroto.rb
+++ examples/module_maroto.rb
@@ -1,9 +1,15 @@
-require "optioning"
+begin
+  require "optioning"
+rescue LoadError => e
+  $:.unshift File.expand_path "../../lib", __FILE__
+  require "optioning"
+end
 
 module Maroto
   def hasherize(*values_and_options)
     optioning = Optioning.new values_and_options
     optioning.deprecate :to_hash, :to, "v2.0.0"
-    optioning.deprecation_warn caller
+    optioning.recognize :persist
+    optioning.process caller
   end
 end
